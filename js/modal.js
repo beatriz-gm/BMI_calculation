a087d83dc4bb7fac7855df2d84b5046c114fc4cc
Beatriz <104030147+beatriz-gm@users.noreply.github.com>
2023-09-04 08:54:27 -0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
+}
+
+Modal.buttonClose.onclick = () => Modal.close()
+
+
+export {Modal}
+
+window.addEventListener('keydown', handleKeydown)
+function handleKeydown(event) {
+  if (event.key === 'Escape') {
+    Modal.close()
+  }
+}
